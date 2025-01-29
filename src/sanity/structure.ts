8399bdfa2388500ddf427b7646ad4ccebcedec0b
Aqsa <aqsagulamhasan@gmail.com>
2025-01-30 01:30:08 +0500

    Changes

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,4 +1,6 @@
-export const structure = (S) =>
+import { StructureResolver } from "sanity/structure";
+
+export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
     .items(S.documentTypeListItems())
